67042a512e689ad508c62f27dbb5d6c629047c2b
Shandukani Radzilani <165278788+shandukaniradzilani@users.noreply.github.com>
2024-11-21 14:09:57 +0200

    App.tsx updates

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationAction, NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from './HomeScreen';
+import AddMenuscreen from './AddMenuItemScreen';
+import FilterMenuScreen from './FilterMenuScreen';
+import React from "react";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Add Menu" component={AddMenuscreen}/>
+        <Stack.Screen name="Filter Menu" component={FilterMenuScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
