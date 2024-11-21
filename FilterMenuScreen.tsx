4118098f71475cef1fda164a5d083e99853c9c97
Shandukani Radzilani <165278788+shandukaniradzilani@users.noreply.github.com>
2024-11-21 15:18:00 +0200

    filter update

---
+++ FilterMenuScreen.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { View, Text, FlatList, Button, StyleSheet } from "react-native";
+
+const FilterMenuScreen = ({ route, navigation }: any) => {
+    const { menuItems } = route.params;
+    const [filterItems, setFilteredItems ] = useState(menuItems);
+    const filterByCourse = (course: string) => {
+        setFilteredItems(menuItems.filter((item: { course: string}) => item.course === course));
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Course</Text>
+            <Button title="Starter" onPress={() => filterByCourse('Starter')} />
+            <Button title="Mains" onPress={() => filterByCourse('Main')} />
+            <Button title="desserts" onPress={() => filterByCourse('Desserts')} />
+            
+            <FlatList 
+            data={filterItems}
+            keyExtractor={(item) => item.id}
+            renderItem={()}
+
+        </View>
+    )
+}
